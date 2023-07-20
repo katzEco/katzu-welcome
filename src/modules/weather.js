d1756c37a20d341c02b1d2788039a6aa9c1b7b9e
Suphakit P <me@suphakit.net>
2023-07-21 04:48:29 +0700

    chore<modules>: weather function

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
 
 const city = 'Phayao'
-const link = `wttr.in/${city}?format=1`
+const link = `https://wttr.in/${city}?format=1`
 
 async function weather() {
   const call = await axios.get(link)
 
-  console.log(call)
+  return call.data
 }
 
-module.exports = [weather]
+module.exports = weather
