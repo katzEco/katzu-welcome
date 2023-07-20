650486afc838d772afad9affcd3f5f5e4c6a2ef5
Suphakit P <me@suphakit.net>
2023-07-21 04:50:34 +0700

    chore<art_scripts>: rando color function

--- src/modules/art.js
+++ src/modules/art.js
@@ -1,43 +1,7 @@
 const pkgFace = require("cool-ascii-faces");
-const color = require("bash-color");
-
-function randomColors(inp) {
-  let randomizer = Math.floor(Math.random() * 9);
-
-  switch (randomizer) {
-    case 1:
-      return color.yellow(inp, true)
-
-    case 2:
-      return color.blue(inp)
-
-    case 3:
-      return color.cyan(inp)
-
-    case 4:
-      return color.green(inp)
-
-    case 5:
-      return color.purple(inp)
-
-    case 6:
-      return color.red(inp)
-
-    case 7:
-      return color.yellow(inp)
-
-    case 8:
-      return color.red(inp, true) 
-
-    default:
-      return color.white(inp)
-  }
-}
 
 function faces() {
-  const rtn = randomColors(pkgFace());
-
-  return rtn
+  return pkgFace()
 }
 
 module.exports = faces;
